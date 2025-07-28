47a8d87f2ca69ea8f0937b0bc00a048d9995629b
Rajpriya <rajpriyavondillin1105@gmail.com>
2025-07-28 18:02:21 +0530

    Updated the changes

--- July-28_Practices_and_Assessments/DB_Connection/Practice.py
+++ July-28_Practices_and_Assessments/DB_Connection/Practice.py
@@ -11,30 +11,30 @@ conn=mysql.connector.connect(
 
 cursor=conn.cursor()
 
-# #Step 2: Create a table - commit() is not required
-# create_table_query="""
-# CREATE TABLE IF NOT EXISTS employees (
-#     id INT AUTO_INCREMENT PRIMARY KEY,
-#     name VARCHAR(100),
-#     department VARCHAR(100),
-#     salary FLOAT
-# );
-# """
-#
-# cursor.execute(create_table_query)
-# print("Table 'employees' created successfully.")
+#Step 2: Create a table - commit() is not required
+ create_table_query="""
+ CREATE TABLE IF NOT EXISTS employees (
+     id INT AUTO_INCREMENT PRIMARY KEY,
+     name VARCHAR(100),
+     department VARCHAR(100),
+     salary FLOAT
+ );
+ """
+
+ cursor.execute(create_table_query)
+ print("Table 'employees' created successfully.")
 
 #Step 3: Insert sample data
-# insert_query="""
-# INSERT INTO employees (name,department,salary)
-# VALUES
-#     ('Rahul Sharma','Engineering',75000),
-#     ('Priya Verma','Marketing',60000),
-#     ('Anil Kapoor','HR',50000);
-# """
-# cursor.execute(insert_query)
-# conn.commit()
-# print("Sample data inserted successfully.")
+ insert_query="""
+ INSERT INTO employees (name,department,salary)
+ VALUES
+     ('Rahul Sharma','Engineering',75000),
+     ('Priya Verma','Marketing',60000),
+     ('Anil Kapoor','HR',50000);
+"""
+cursor.execute(insert_query)
+conn.commit()
+print("Sample data inserted successfully.")
 
 # Update and Delete
 update_query="""UPDATE employees SET salary = 55000 WHERE department='HR';"""
